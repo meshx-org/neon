50c1d885713d34a65e1563f4f031e0dc2fe409aa
Konstantin Knizhnik <knizhnik@neon.tech>
2023-12-25 23:05:49 +0200

    Add extra trace

--- pgxn/neon/pagestore_smgr.c
+++ pgxn/neon/pagestore_smgr.c
@@ -2733,7 +2733,6 @@ static int
 neon_read_slru_segment(SMgrRelation reln, SlruKind kind, int segno, void* buffer)
 {
 	XLogRecPtr request_lsn;
-	/* TODO: any better alternative than flush LSN? Actually we to request SLRU at basebackup creation time... */
 	request_lsn = GetRedoStartLsn();
 	request_lsn = nm_adjust_lsn(request_lsn);
 
@@ -2758,6 +2757,7 @@ neon_read_slru_segment(SMgrRelation reln, SlruKind kind, int segno, void* buffer
 	{
 		case T_NeonGetSlruSegmentResponse:
 			n_blocks = ((NeonGetSlruSegmentResponse *) resp)->n_blocks;
+			elog(LOG, "Read %d blocks of SLRU %d segment %d", n_blocks, kind, segno);
 			memcpy(buffer, ((NeonGetSlruSegmentResponse *) resp)->data, n_blocks*BLCKSZ);
 			break;
 
