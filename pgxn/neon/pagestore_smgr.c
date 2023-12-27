d3ab577ab2143b432de2a4b86c8333c196b21c93
Konstantin Knizhnik <knizhnik@neon.tech>
2023-12-27 14:10:35 +0200

    Undo extra log messages

--- pgxn/neon/pagestore_smgr.c
+++ pgxn/neon/pagestore_smgr.c
@@ -2736,8 +2736,6 @@ neon_read_slru_segment(SMgrRelation reln, SlruKind kind, int segno, void* buffer
 	request_lsn = GetRedoStartLsn();
 	request_lsn = nm_adjust_lsn(request_lsn);
 
-	elog(WARNING, "neon_read_slru_segment is called for segment %d of SLRU %d", kind, segno);
-
 	NeonResponse *resp;
 	NeonGetSlruSegmentRequest request = {
 		.req.tag = T_NeonGetSlruSegmentRequest,
@@ -2759,7 +2757,6 @@ neon_read_slru_segment(SMgrRelation reln, SlruKind kind, int segno, void* buffer
 	{
 		case T_NeonGetSlruSegmentResponse:
 			n_blocks = ((NeonGetSlruSegmentResponse *) resp)->n_blocks;
-			elog(WARNING, "Read %d blocks of SLRU %d segment %d", n_blocks, kind, segno);
 			memcpy(buffer, ((NeonGetSlruSegmentResponse *) resp)->data, n_blocks*BLCKSZ);
 			break;
 
